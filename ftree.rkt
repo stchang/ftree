7c98759d5378cc8d72a16cff4fa37ff169296771
Stephen Chang <stchang@ccs.neu.edu>
2013-05-17 19:28:15 -0400

    add concat; tests not working

--- ftree.rkt
+++ ftree.rkt
@@ -2,7 +2,15 @@
 
 (provide empty-ft ft-empty? ft-cons ft-headL ft-tailL viewL ViewL)
 
-; Finger trees, from Hinze and Paterson 2006 JFP paper
+; 2-3 finger trees, from Hinze and Paterson 2006 JFP paper
+;; implements functional, persistent sequences 
+;; - amortized constant time access to either end
+;; - log (in size of smaller piece) time concatentation and splitting
+
+;; TODO: [o] = open, [x] = done
+;; [o] 2013-05-15: in FTree, replace middle FTree of Node3's 
+;;       with FTree of Three's
+
 
 ;;; A [23Treeof X] is one of:
 ;;; - (Zero)
@@ -23,7 +31,7 @@
 ;; An [FTreeof X] is one of:
 ;; - empty-ft
 ;; - (Single x)
-;; - (Deep Digit [FTreeof Node] Digit)
+;; - (Deep Digit [FTreeof Node3] Digit)
 
 (define empty-ft null)
 (struct Single (x)) ;; analogous to Okasaki's Shallow
@@ -53,28 +61,28 @@
 
 ;; ft-cons: insert new element on the left of given ftree
 (define (ft-cons a ft)
-  (if (ft-empty? ft) (Single a)
+  (if (ft-empty? ft)                     (Single a)
       (match ft
         [(Single b)                      (Deep (One a) empty-ft (One b))]
+        [(Deep (One b) mid right)        (Deep (Two a b) mid right)]
+        [(Deep (Two b c) mid right)      (Deep (Three a b c) mid right)]
+        [(Deep (Three b c d) mid right)  (Deep (Four a b c d) mid right)]
         [(Deep (Four b c d e) mid right) (Deep (Two a b)
                                                (ft-cons (Node3 c d e) mid)
-                                               right)]
-        [(Deep (Three b c d) mid right)  (Deep (Four a b c d) mid right)]
-        [(Deep (Two b c) mid right)      (Deep (Three a b c) mid right)]
-        [(Deep (One b) mid right)        (Deep (Two a b) mid right)])))
+                                               right)])))
 
 ;; ft-snoc: insert new element on the right of given ftree
 (define (ft-snoc ft a)
-  (if (ft-empty? ft) (Single a)
+  (if (ft-empty? ft)                    (Single a)
       (match ft
         [(Single b)                     (Deep (One b) empty-ft (One a))]
+        [(Deep left mid (One b))        (Deep left mid (Two b a))]
+        [(Deep left mid (Two c b))      (Deep left mid (Three c b a))]
+        [(Deep left mid (Three d c b))  (Deep left mid (Four d c b a))]
         [(Deep left mid (Four e d c b)) (Deep left
                                               ;; just use Three instead of Node3?
                                               (ft-snoc mid (Node3 e d c))
-                                              (Two b a))]
-        [(Deep left mid (Three d c b))  (Deep left mid (Four d c b a))]
-        [(Deep left mid (Two c b))      (Deep left mid (Three c b a))]
-        [(Deep left mid (One b))        (Deep left mid (Two b a))])))
+                                              (Two b a))])))
 
 ;; An [FTreeViewLof X] is one of:
 ;; - empty-view
@@ -86,13 +94,13 @@
 
 ;; viewL : [FTreeof X] -> [FTreeViewLof X]
 (define (viewL ft)
-  (if (ft-empty? ft) empty-view
+  (if (ft-empty? ft)                     empty-view
       (match ft
-        [(Single x)                      (ViewL x empty-ft)]
-        [(Deep (One x) mid right)        (ViewL x (mk-deepL null mid right))]
-        [(Deep (Two x y) mid right)      (ViewL x (mk-deepL (One y) mid right))]
-        [(Deep (Three x y z) mid right)  (ViewL x (mk-deepL (Two y z) mid right))]
-        [(Deep (Four w x y z) mid right) (ViewL w (mk-deepL (Three x y z) mid right))])))
+        [(Single a)                      (ViewL a empty-ft)]
+        [(Deep (One a) mid right)        (ViewL a (mk-deepL null mid right))]
+        [(Deep (Two a b) mid right)      (ViewL a (mk-deepL (One b) mid right))]
+        [(Deep (Three a b c) mid right)  (ViewL a (mk-deepL (Two b c) mid right))]
+        [(Deep (Four a b c d) mid right) (ViewL a (mk-deepL (Three b c d) mid right))])))
 
 ;; constructs the appropriate Deep ftree, 
 ;; according to the first arg, which can be a Digit or null
@@ -105,12 +113,12 @@
       (Deep digit-or-null mid right)))
 
 ;; converts digit to ftree
-(define (digit->ftreeL d)
-  (match d
-    [(One x)        (Single x)]
-    [(Two x y)      (Deep (One x) empty-ft (One y))]
-    [(Three x y z)  (Deep (Two x y) empty-ft (One z))]
-    [(Four w x y z) (Deep (Three w x y) empty-ft (One z))]))
+(define (digit->ftreeL dig)
+  (match dig
+    [(One a)        (Single a)]
+    [(Two a b)      (Deep (One a) empty-ft (One b))]
+    [(Three a b c)  (Deep (Two a b) empty-ft (One c))]
+    [(Four a b c d) (Deep (Three a b) empty-ft (One c d))]))
 
 (define (node3->digit n) (match n [(Node3 x y z) (Three x y z)]))
 (define (ft-headL ft) (ViewL-x (viewL ft)))
@@ -153,3 +161,22 @@
 (define (ft-headR ft) (ViewR-x (viewR ft)))
 (define (ft-tailR ft) (ViewR-tree (viewR ft)))
 
+
+;; concatenation
+(define (ft-append ft1 ft2)
+  (cond [(ft-empty? ft1) ft2]
+        [(ft-empty? ft2) ft1]
+        [else 
+         (match* (ft1 ft2)
+           [((Single a) _) (ft-cons a ft2)]
+           [(_ (Single a)) (ft-snoc ft1 a)]
+           [((Deep left1 mid1 right1) (Deep left2 mid2 right2))
+            (Deep left1 (ft-append (ft-snoc mid1 right1) (ft-cons left2 mid2)) right2)])]))
+  
+
+;; A [Splitof X] is a (Split X X)
+(struct Split (left right))
+(struct Split3 (left x right))
+
+
+      
